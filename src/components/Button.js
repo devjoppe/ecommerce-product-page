5fe61640f47911b06b9b8d486b553fab67aa64c4
devjoppe <joakim.ottosson@medieinstitutet.se>
2022-10-18 14:16:01 +0200

    Add to cart design done

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-let mainButton = 'mainbutton ';
-
 const Button = ({ buttonText, buttonWidth, buttonIcon, buttonType }) => {
   return (
     <button style={{width: buttonWidth}} className={buttonType}><FontAwesomeIcon icon={buttonIcon} className='buttonicon' />{buttonText}</button>
