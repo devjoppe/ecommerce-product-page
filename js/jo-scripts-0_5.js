a11bfe8b442ecfd7c141f3ecb5b4951eacf4a418
devjoppe <joakim.ottosson@medieinstitutet.se>
2022-10-21 10:48:32 +0200

    function for adding items and calculating the price is done

--- js/jo-scripts-0_5.js
+++ js/jo-scripts-0_5.js
@@ -1,9 +1,16 @@
-let addButton = document.getElementById("itemadd").id;
-let removeButton = document.getElementById("itemremove").id;
-
-console.log(addButton, removeButton);
+let itemResult = 1;
+let itemPrice = 259;
 
 // Function to add and remove items from Product bar. And calculate the price.
-const addRemove = () => {
-    
+const addRemove = btnType => {
+    if (btnType == 'add') {
+        itemResult++
+        itemPrice += 259;
+    } else if (btnType == 'remove' && itemResult >= 2) {
+        itemResult--
+        itemPrice -= 259;
+    }
+    document.getElementById("itemresult").innerHTML = itemResult;
+    document.getElementById("itemprice").innerHTML = itemPrice;
+    console.log(itemResult);
 }
