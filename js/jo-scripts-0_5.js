98669e4330d901f5cd323ca6f78a312f6e4160b4
devjoppe <joakim.ottosson@medieinstitutet.se>
2022-10-21 10:23:41 +0200

    javascript for add remove items

--- js/jo-scripts-0_5.js
+++ js/jo-scripts-0_5.js
@@ -1,1 +1,9 @@
-// Skriver min javascript
+let addButton = document.getElementById("itemadd").id;
+let removeButton = document.getElementById("itemremove").id;
+
+console.log(addButton, removeButton);
+
+// Function to add and remove items from Product bar. And calculate the price.
+const addRemove = () => {
+    
+}
